259b343957d1c9c1c60023009339f9c5768be341
Lars Kristiansen <larkr23@student.sdu.dk>
2024-02-17 17:11:23 +0100

    table and first blog

--- src/main/java/org/example/blog/BlogApplication.java
+++ src/main/java/org/example/blog/BlogApplication.java
@@ -1,18 +1,22 @@
 package org.example.blog;
 
+import org.example.blog.database.SQLGetter;
 import org.example.blog.database.database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-    public static database db = new database("localhost", "root", "dasd", "blog", 3306);
+    public static database db = new database("sql11.freesqldatabase.com", "sql11684801", "lD5Ndr2Gxa", "sql11684801", 3306);
 
     public static void main(String[] args) {
 
         SpringApplication.run(BlogApplication.class, args);
         if(!db.isConnected()) {
             db.connect();
+            SQLGetter sqlGetter = new SQLGetter();
+            sqlGetter.createTable();
+            sqlGetter.createBlog();
         }
 
     }
--- src/main/java/org/example/blog/database/SQLGetter.java
+++ src/main/java/org/example/blog/database/SQLGetter.java
@@ -7,12 +7,22 @@ import java.sql.SQLException;
 
 public class SQLGetter {
     private PreparedStatement statement;
+    private String createBlog1 = "INSERT INTO blog (title, content) values ('My first blog', 'In this my first blog im not going to write much')";
+    private String createTableSQL1 = "CREATE TABLE IF NOT EXISTS blog (id SERIAL PRIMARY KEY, title varchar(255), content text)";
 
     public void createTable() {
         try {
-            statement = BlogApplication.db.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS blog (id int NOT NULL AUTO_INCREMENT, title text, content text, PRIMARY KEY (id))");
+            statement = BlogApplication.db.getConnection().prepareStatement(createTableSQL1);
             statement.executeUpdate();
         }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void createBlog() {
+        try {
+            statement =  BlogApplication.db.getConnection().prepareStatement(createBlog1);
+            statement.executeUpdate();
+        }catch (SQLException e) {
             e.printStackTrace();
         }
     }
